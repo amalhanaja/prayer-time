d54ed4cd57724ef555df838f0fba3ad4edcf0255
Alfian Akmal Hanantio <amalhanaja@gmail.com>
2018-05-19 04:28:27 +0700

    Remove NONE

--- src/main/kotlin/io/github/amalhanaja/prayertime/entity/Prayer.kt
+++ src/main/kotlin/io/github/amalhanaja/prayertime/entity/Prayer.kt
@@ -1,13 +1,11 @@
 package io.github.amalhanaja.prayertime.entity
 
 enum class Prayer {
-    NONE,
     IMSAK,
     FAJR,
     SUNRISE,
     DHUHR,
     ASR,
-//    SUNSET,
     MAGHRIB,
     ISHA
 }
